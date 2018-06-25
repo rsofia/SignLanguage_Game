6f03d46ee145b15d78b6c718f4b3dd9a08446804
valera0823 <luis_luis_260@hotmail.com>
2018-06-25 15:55:25 -0500

    Cambios a cosas del json
    falta agregar el guardado a la clase principal y metodo de tiraje

--- Assets/valera/newDetection_/s_testJson.cs
+++ Assets/valera/newDetection_/s_testJson.cs
@@ -23,32 +23,37 @@ public class s_testJson : MonoBehaviour {
     }
 
     public testClass asd = new testClass(0,"asd",new Vector3(1,2,3));
-    public testClass asd2 = new testClass(3, "dfsdf", new Vector3(23, 23, 33));
+    public testClass asd1 = new testClass(1, "asdasd", new Vector3(221, 32, 43));
+
+    public testClass asd2 = new testClass(2, "2", new Vector3(2, 2, 2));
+    public testClass asd3 = new testClass(2, "2", new Vector3(2, 2, 2));
+
 
     void Start ()
     {
         string json = JsonUtility.ToJson(asd);
-        print(json);
+        string json2 = JsonUtility.ToJson(asd1);
+
+
         if (!Directory.Exists("TestJson"))
             Directory.CreateDirectory("TestJson");
         string path = "TestJson/test.json";
 
+        
+
         //Write some text to the test.txt file
-        StreamWriter writer = new StreamWriter(path, true);
+        StreamWriter writer = new StreamWriter(path,false);
         writer.WriteLine(json);
+        writer.WriteLine(json2);
+
         writer.Close();
 
-
-        print(asd2.id);
 
         //Read the text from directly from the test.txt file
         StreamReader reader = new StreamReader(path);
         asd2 = JsonUtility.FromJson<testClass>(reader.ReadLine());
+        asd3 = JsonUtility.FromJson<testClass>(reader.ReadLine());
         reader.Close();
-
-
-
-        print(asd2.id);
     }
 
     void Update ()
