8718cec2ea59f5f969101dfce978732bef6d6e60
valera0823 <luis_luis_260@hotmail.com>
2018-07-09 01:06:15 -0500

    Sistema de guardado funcional
    Se agregan los parametros para guardar archivos json es funcional.
    ->Falta cambiar[,] da error al guardar

--- Assets/valera/newDetection_/s_ManagerExtend.cs
+++ Assets/valera/newDetection_/s_ManagerExtend.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Leap.Unity.Attributes;
-
+using System.IO;
+using UnityEngine.UI;
 
 namespace Leap.Unity
 {
@@ -101,9 +102,13 @@ namespace Leap.Unity
             v_RotBonesPinky  = new Vector3[3, v_NumeroDeTirajes];
             v_RotBonesRing   = new Vector3[3, v_NumeroDeTirajes];
 
-            v_PosRotBrazo    = new Vector3[2, v_NumeroDeTirajes];
+            v_PosRotPalma    = new Vector3[2, v_NumeroDeTirajes];
             v_PosRotBrazo    = new Vector3[2, v_NumeroDeTirajes];
 
+         
+
+
+            Debug.Log(v_NumeroDeTirajes);
         }
 
     }
@@ -116,7 +121,7 @@ namespace Leap.Unity
         public float v_periodoActualizacion = 0.1f;
         public TiemposDisponibles v_TiempoDeGrabado;
         public CiclosGravado v_CiclosDeGravacion;
-
+        public InputField v_NombreArchivo;
 
         [Header("Requerimientos")]
         [Tooltip("The hand model to watch. Set automatically if detector is on a hand.")]
@@ -129,8 +134,8 @@ namespace Leap.Unity
         public bool v_ManoDerechaActiva;
 
         [Header("Datos a Guardar")]
-        public HandRecord v_ManoIzquierda = new HandRecord(1);
-        public HandRecord v_ManoDerecha = new HandRecord(1);
+        public HandRecord v_ManoIzquierda;
+        public HandRecord v_ManoDerecha;
 
         [Header("Datos de Visualizacion")]
         public string v_PosicionManoIzquierda;
@@ -150,12 +155,43 @@ namespace Leap.Unity
         public int v_EstadoMiddleDerecho;
         public int v_EstadoPinkyDerecho;
         public int v_EstadoRingDerecho;
+        [Header("Objetos de ref Mano izquierda")]
+        public GameObject[] ThumbI;
+        public GameObject[] IndexI;
+        public GameObject[] MiddleI;
+        public GameObject[] PinkyI;
+        public GameObject[] RingI;
+
+        [Header("Objetos de ref Mano Derecha")]
+        public GameObject[] ThumbD;
+        public GameObject[] IndexD;
+        public GameObject[] MiddleD;
+        public GameObject[] PinkyD;
+        public GameObject[] RingD;
 
 
         private IEnumerator watcherCoroutine;
 
-        void Start() {
-
+        void Start()
+        {
+            int _tirajes = 0;
+            switch (v_CiclosDeGravacion)
+            {
+                case CiclosGravado._5Times:
+                    _tirajes = 5;
+                    break;
+                case CiclosGravado._10Times:
+                    _tirajes = 10;
+                    break;
+                case CiclosGravado._15Times:
+                    _tirajes = 15;
+                    break;
+                case CiclosGravado._20Times:
+                    _tirajes = 20;
+                    break;
+            }
+            v_ManoIzquierda = new HandRecord(_tirajes);
+            v_ManoDerecha = new HandRecord(_tirajes);
         }
         void Awake()
         {
@@ -165,7 +201,8 @@ namespace Leap.Unity
         {
             StartCoroutine(watcherCoroutine);
         }
-        void Update() {
+        void Update()
+        {
 
         }
 
@@ -176,44 +213,142 @@ namespace Leap.Unity
             int _tirajes = 0;
             int _tiempoEnTiraje = 0;
 
-            //Agregar Metodo para convertir los Enums a datos validos para ser procesados
+            switch(v_TiempoDeGrabado)
+            {
+                case TiemposDisponibles._1Segundos:
+                    _tiempoEnTiraje = 1;
+                    break;
+                case TiemposDisponibles._2Segundos:
+                    _tiempoEnTiraje = 2;
+                    break;
+                case TiemposDisponibles._5Segundos:
+                    _tiempoEnTiraje = 5;
+                    break;
+                case TiemposDisponibles._10Segundos:
+                    _tiempoEnTiraje = 10;
+                    break;
+            }
 
+            switch(v_CiclosDeGravacion)
+            {
+                case CiclosGravado._5Times:
+                    _tirajes = 5;
+                    break;
+                case CiclosGravado._10Times:
+                    _tirajes = 10;
+                    break;
+                case CiclosGravado._15Times:
+                    _tirajes = 15;
+                    break;
+                case CiclosGravado._20Times:
+                    _tirajes = 20;
+                    break;
+            }
+            Debug.Log("Se activo corutina de guardado");
             StartCoroutine(fn_GuardarDatos(_tirajes, _tiempoEnTiraje));
         }
 
         #region
-            public void fn_GuardarDatosJSonFormat()
-            {
-                //funcion para guardar toda la clase de forma de json 
-                //formato standart para poder hacer una lectura de forma nativa
-            }
+        public void fn_GuardarDatosJSonFormat(string _nombreGesto)
+        {
+
+            string jsonManoIzquierda = JsonUtility.ToJson(v_ManoIzquierda);
+            string jsonManoDerecha = JsonUtility.ToJson(v_ManoDerecha);
+
+            if (!Directory.Exists("GestosJson"))
+                Directory.CreateDirectory("GestosJson");
+
+            string path = "GestosJson/" + _nombreGesto + ".json";
+
+            StreamWriter writer = new StreamWriter(path, false);
+            writer.WriteLine(jsonManoIzquierda);
+            writer.WriteLine(jsonManoDerecha);
+
+            writer.Close();
+        }
         
-            public void fn_GuardarDatosBinaryFormat()
-            {
-                //funcion de respaldo para guardar datos de forma binaria
-                //de ser necesario agregarla
-            }    
         #endregion
 
         #region ///Corutinas
 
         IEnumerator fn_GuardarDatos(int _numeroTirajes, float _tiempoEntreTirajes)
         {
+   
+            float tiempoTMp = _tiempoEntreTirajes / _numeroTirajes;
+            Debug.Log(tiempoTMp);
 
-            ///agregar condicionales para llenar ambas manos o solo 1 mano siendo el caso
-            ///Guardar datos individuales de cada parametro en la clase correspondiente
 
-            for(int i = 0; i < _numeroTirajes; i++)
+            for (int i = 0; i < _numeroTirajes; i++)
             {
+                Debug.Log("Tiraje"+i);
+                if(v_ManoIzquierdaActiva)
+                {
+                    //Agregar tirajes de estados de cada dedo y cantidades de dedos.
+                    v_ManoIzquierda.v_ManoActiva = true;
 
-                yield return new WaitForSeconds(_numeroTirajes);
+                    v_ManoIzquierda.v_RotacionPalma = v_PosicionManoIzquierda;
+
+                    v_ManoIzquierda.v_EstadoThumb = v_EstadoThumbIzquierdo;
+                    v_ManoIzquierda.v_EstadoIndex = v_EstadoIndexIzquierdo;
+                    v_ManoIzquierda.v_EstadoMiddle = v_EstadoMiddleIzquierdo;
+                    v_ManoIzquierda.v_EstadoPinky = v_EstadoPinkyIzquierdo;
+                    v_ManoIzquierda.v_EstadoRing = v_EstadoRingIzquierdo;
+
+                    v_ManoIzquierda.v_DedosExtendidos = v_NumeroDeDedosActivosHIzquierda;
+
+
+                    //cambiar sistema de clase para poder enviar transform y rotation
+                    //cambio de sistema [,] error al guardar informacion 
+
+                    v_ManoIzquierda.v_PosBonesThumb[0, i] = ThumbI[0].transform.position; 
+                    v_ManoIzquierda.v_PosBonesThumb[1, i] = ThumbI[1].transform.position; 
+                    v_ManoIzquierda.v_PosBonesThumb[2, i] = ThumbI[2].transform.position;
+
+                    
+                }
+                else
+                {
+                    v_ManoIzquierda.v_ManoActiva = false;
+                    Debug.Log("Mano izquierda perdida");
+
+                }
+
+                if (v_ManoDerechaActiva)
+                {
+                    //Agregar tirajes de estados de cada dedo y cantidades de dedos.
+
+                    v_ManoDerecha.v_ManoActiva = true;
+
+                    v_ManoDerecha.v_RotacionPalma = v_PosicionManoDerecha;
+
+                    v_ManoDerecha.v_EstadoThumb = v_EstadoThumbDerecho;
+                    v_ManoDerecha.v_EstadoIndex = v_EstadoIndexDerecho;
+                    v_ManoDerecha.v_EstadoMiddle = v_EstadoMiddleDerecho;
+                    v_ManoDerecha.v_EstadoPinky = v_EstadoPinkyDerecho;
+                    v_ManoDerecha.v_EstadoRing = v_EstadoRingDerecho;
+
+                    v_ManoDerecha.v_DedosExtendidos = v_NumeroDeDedosActivosHDerecha;
+
+                    //cambiar sistema de clase para poder enviar transform y rotation
+                    //cambio de sistema [,] error al guardar informacion 
+
+                }
+                else
+                {
+                    v_ManoDerecha.v_ManoActiva = false;
+                    Debug.Log("Mano Derecha perdida");
+
+                }
+
+
+                yield return new WaitForSeconds(tiempoTMp);
             }
 
 
             //agregar metodo para guardar de forma local los datos de las clases
             //json / binary format
-
-
+            Debug.Log("Se acciona el guardado en json");
+            fn_GuardarDatosJSonFormat(v_NombreArchivo.text);
             Debug.Log("Termino de GuardarInformacion");
         }
 
